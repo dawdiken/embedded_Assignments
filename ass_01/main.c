d6ac7e30599e0a2c5e10e96422330c0f443d13e4
DAVID KENNY - Student <G00070718@gmit.ie>
2018-01-26 16:12:34 +0000

    first commit

--- ass_01/main.c
+++ ass_01/main.c
@@ -5,13 +5,10 @@ Student Num:	G00070718
 date			25/01/2018
 Description:	Assignment 01
 */
-
-
 #define _CRT_SECURE_NO_WARNINGS
 #define Num_of_Structs 4 
 
 #include "tmpMon.h"
-
 
 
 int main()
@@ -19,12 +16,11 @@ int main()
 	distance tempvalues[Num_of_Structs]; //make 4 structures here
 	
 	fill_My_Structs(&tempvalues); //pass structures to function to be populated with data
-		
-	//tempvalues[1].printer_operation(&tempvalues[0], &tempvalues[1], &tempvalues[2], &tempvalues[3]);
-	//int struct_num = 0;//variable to pass each struct
-	//tempvalues->printer_operation(&tempvalues[struct_num], &tempvalues[struct_num+1], &tempvalues[struct_num+1], &tempvalues[struct_num+1]);
-	tempvalues->printer_operation(&tempvalues);
-	calculate_Temp(&tempvalues);	//print average temp of all readings 
+
+	tempvalues->printer_operation(&tempvalues);//call the 
+	
+	calculate_Temp(&tempvalues);	//call function which callculate the average temp of all readings and print it 
+	
 	return 0;
 }
 
